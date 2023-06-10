ad46a73384541736b3431db453a36f1346ba8a40
rzdtjb <rzdtjb@gmail.com>
2023-06-10 22:03:22 +0800

    表现层数据封装

--- src/main/java/com/itheima/controller/UserController.java
+++ src/main/java/com/itheima/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.itheima.controller;
 
 import com.github.pagehelper.PageInfo;
+import com.itheima.controller.results.Code;
+import com.itheima.controller.results.Result;
 import com.itheima.domain.User;
 import com.itheima.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/user")
@@ -17,33 +19,34 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    public boolean save(User user){
-        return userService.save(user);
+    public Result save(User user) {
+        boolean flag = userService.save(user);
+        return new Result(flag ? Code.SAVE_OK : Code.SAVE_ERROR);
     }
 
     @PutMapping
-    public boolean update(User user){
+    public boolean update(User user) {
         return userService.update(user);
     }
 
     @DeleteMapping("/{uuid}")
-    public boolean delete(@PathVariable Integer uuid){
+    public boolean delete(@PathVariable Integer uuid) {
         return userService.delete(uuid);
     }
 
     @GetMapping("/{uuid}")
-    public User get(@PathVariable Integer uuid){
+    public User get(@PathVariable Integer uuid) {
         return userService.get(uuid);
     }
 
     @GetMapping("/{page}/{size}")
-    public PageInfo<User> getAll(@PathVariable Integer page, @PathVariable Integer size){
-        return userService.getAll(page,size);
+    public PageInfo<User> getAll(@PathVariable Integer page, @PathVariable Integer size) {
+        return userService.getAll(page, size);
     }
 
     @PostMapping("/login")
-    public User login(String userName,String password){
-        return userService.login(userName,password);
+    public User login(String userName, String password) {
+        return userService.login(userName, password);
     }
 
 }
---
+++ src/main/java/com/itheima/controller/results/Result.java
@@ -0,0 +1,45 @@
+package com.itheima.controller.results;
+
+public class Result {
+    private Integer code;
+    private Object data;
+    private String message;
+
+    public Result(Integer code) {
+        this.code = code;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "code=" + code +
+                ", data=" + data +
+                ", message='" + message + '\'' +
+                '}';
+    }
+
+}
